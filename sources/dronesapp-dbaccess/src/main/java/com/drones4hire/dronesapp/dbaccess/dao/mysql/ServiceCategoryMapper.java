3b5a3334bf3f71deb8a43f6606169164254051b0
boniara <brutskov@onflydev.com>
2017-05-12 15:03:08 +0300

    added service category dao layer, added idea cash files to .gitignore file

---
+++ sources/dronesapp-dbaccess/src/main/java/com/drones4hire/dronesapp/dbaccess/dao/mysql/ServiceCategoryMapper.java
@@ -0,0 +1,18 @@
+package com.drones4hire.dronesapp.dbaccess.dao.mysql;
+
+import com.drones4hire.dronesapp.models.db.services.ServiceCategory;
+
+import java.util.List;
+
+public interface ServiceCategoryMapper {
+
+    void create(ServiceCategory serviceCategory);
+
+    ServiceCategory getServiceCategoryById(long id);
+
+    List<ServiceCategory> getAllServiceCategories();
+
+    void updateServiceCategory(ServiceCategory serviceCategory);
+
+    void deleteServiceCategory(long id);
+}
