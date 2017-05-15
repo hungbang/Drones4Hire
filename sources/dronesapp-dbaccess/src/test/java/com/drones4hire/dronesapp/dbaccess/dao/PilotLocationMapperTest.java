8af7343cc185afc7dcd8c4869b932576150ce156
boniara <brutskov@onflydev.com>
2017-05-15 12:32:24 +0300

    fixed select sql bug

--- sources/dronesapp-dbaccess/src/test/java/com/drones4hire/dronesapp/dbaccess/dao/PilotLocationMapperTest.java
+++ sources/dronesapp-dbaccess/src/test/java/com/drones4hire/dronesapp/dbaccess/dao/PilotLocationMapperTest.java
@@ -14,9 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.annotations.Test;
-
 import java.util.List;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotEquals;
 import static org.testng.Assert.assertNull;
