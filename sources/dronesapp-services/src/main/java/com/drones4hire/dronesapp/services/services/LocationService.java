14feb2d8beff633fcb8dd1dae587c2d791092174
boniara <brutskov@onflydev.com>
2017-05-16 13:52:28 +0300

    added location service

---
+++ sources/dronesapp-services/src/main/java/com/drones4hire/dronesapp/services/services/LocationService.java
@@ -0,0 +1,44 @@
+package com.drones4hire.dronesapp.services.services;
+
+import com.drones4hire.dronesapp.dbaccess.dao.mysql.LocationMapper;
+import com.drones4hire.dronesapp.models.db.commons.Location;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class LocationService
+{
+
+	@Autowired
+	private LocationMapper locationMapper;
+
+	@Transactional(rollbackFor = Exception.class)
+	public Location createLocation(Location location) {
+		locationMapper.createLocation(location);
+		return location;
+	}
+
+	@Transactional(readOnly = true)
+	public Location getLocationById(long id) {
+		return locationMapper.getLocationById(id);
+	}
+
+	@Transactional(readOnly = true)
+	public List<Location> getAllLocations() {
+		return locationMapper.getAllLocations();
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public Location updateLocation(Location location) {
+		locationMapper.updateLocation(location);
+		return location;
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public void deleteLocation(long id) {
+		locationMapper.deleteLocation(id);
+	}
+}
