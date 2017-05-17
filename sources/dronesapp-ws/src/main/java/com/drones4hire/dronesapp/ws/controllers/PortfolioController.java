6a4ce7d7a1eeaaf0f29f756285767e23f6c19302
Aliaksei Khursevich <hursrevich@gmail.com>
2017-05-17 16:45:10 +0300

    - @CrossOrigin added for portolio controller

--- sources/dronesapp-ws/src/main/java/com/drones4hire/dronesapp/ws/controllers/PortfolioController.java
+++ sources/dronesapp-ws/src/main/java/com/drones4hire/dronesapp/ws/controllers/PortfolioController.java
@@ -18,6 +18,7 @@ import javax.validation.Valid;
 
 @Controller
 @Api(value = "Portfolio API")
+@CrossOrigin
 @RequestMapping("api/v1/portfolio")
 public class PortfolioController extends AbstractController
 {
