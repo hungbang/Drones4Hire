ff6c371a8eae996d9c607b15a48161c5231957bf
anazarenko <andrew@onflydev.com>
2017-05-17 11:26:22 +0300

    Adding error codes.

--- sources/dronesapp-models/src/main/java/com/drones4hire/dronesapp/models/dto/auth/CredentialsDTO.java
+++ sources/dronesapp-models/src/main/java/com/drones4hire/dronesapp/models/dto/auth/CredentialsDTO.java
@@ -3,15 +3,18 @@ package com.drones4hire.dronesapp.models.dto.auth;
 import java.io.Serializable;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 public class CredentialsDTO implements Serializable
 {
 	private static final long serialVersionUID = 1567014101763491651L;
 	
-	@NotNull
+	@NotNull(message = "notNull.erorr.message")
+	@Size(max = 10, min = 5, message = "size.error.message")
 	private String email;
 	
-	@NotNull
+	@NotNull(message = "notNull.erorr.message")
+	@Size(max = 30, min = 3, message = "size.error.message")
 	private String password;
 
 	public String getEmail()
