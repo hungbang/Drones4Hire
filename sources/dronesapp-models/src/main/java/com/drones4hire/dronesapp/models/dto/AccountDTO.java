ddca01c40e43f1d3af3bfac2c0377a0587ce0f90
Aliaksei Khursevich <hursrevich@gmail.com>
2017-05-19 11:30:35 +0300

    - AccountDTO extended

--- sources/dronesapp-models/src/main/java/com/drones4hire/dronesapp/models/dto/AccountDTO.java
+++ sources/dronesapp-models/src/main/java/com/drones4hire/dronesapp/models/dto/AccountDTO.java
@@ -1,13 +1,19 @@
 package com.drones4hire.dronesapp.models.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.constraints.NotNull;
 
 import com.drones4hire.dronesapp.models.db.commons.Location;
+import com.drones4hire.dronesapp.models.db.users.Group;
 
 public class AccountDTO extends AbstractDTO
 {
 	private static final long serialVersionUID = -5542006534130010886L;
 
+	private String username;
+	private String email;
 	@NotNull(message = "First name shouldn't be null")
 	private String firstName;
 	@NotNull(message = "Last name shouldn't be null")
@@ -18,6 +24,27 @@ public class AccountDTO extends AbstractDTO
 	private String photoURL;
 	private String introduction;
 	private String summary;
+	private List<Group> groups = new ArrayList<>();
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public void setUsername(String username)
+	{
+		this.username = username;
+	}
+
+	public String getEmail()
+	{
+		return email;
+	}
+
+	public void setEmail(String email)
+	{
+		this.email = email;
+	}
 
 	public String getFirstName()
 	{
@@ -77,5 +104,15 @@ public class AccountDTO extends AbstractDTO
 	public void setSummary(String summary)
 	{
 		this.summary = summary;
+	}
+
+	public List<Group> getGroups()
+	{
+		return groups;
+	}
+
+	public void setGroups(List<Group> groups)
+	{
+		this.groups = groups;
 	}
 }
